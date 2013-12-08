30a91f64e2cd7c99d92616b99b8338b57f26cfe4
Gervasio Marchand <gervasio@marchand.pro>
2013-12-08 01:52:18 -0200

    Importing project

---
+++ IISTask/IISNotifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace IISTask
+{
+    public class IISNotifier : IRegisteredObject
+    {
+        public void Stop(bool immediate)
+        {
+            //do nothing, I only run tasks if I know that they won't take more than a few seconds.
+        }
+
+        public IISNotifier()
+        {
+            HostingEnvironment.RegisterObject(this);
+        }
+
+        public void Finished()
+        {
+            HostingEnvironment.UnregisterObject(this);
+        }
+    }
+}
---
+++ IISTask/IISTaskFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISTask
+{
+    public class IISTaskFactory
+    {
+        public static Task StartNew(Action act)
+        {
+            IISNotifier notif = new IISNotifier();
+            return Task.Factory.StartNew(() => {
+                act.Invoke();
+                notif.Finished();
+            });
+        }
+
+        public static Task StartNew(Action act, TaskCreationOptions options)
+        {
+            IISNotifier notif = new IISNotifier();
+            return Task.Factory.StartNew(() =>
+            {
+                act.Invoke();
+                notif.Finished();
+            }, options);
+        }
+    }
+}
